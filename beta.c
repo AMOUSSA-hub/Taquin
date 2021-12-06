83b15dbb3b4f9f72fd490a1d09bc07659ab65d07
desousaae <etann.de-sousa-alves@etu.u-pec.fr>
2021-12-06 16:10:24 +0100

    fichier affichage bon a completer

--- beta.c
+++ beta.c
@@ -4,6 +4,7 @@
 int main(int argc, char * argv[]){
 		int posx=150,posy=100,i,j=0;
 		int l,h,choix;
+		char *image_selectionnee[16]={("aj1.png"),("Kangoo.jpg"),("Vegeta.jpg")};
 		scanf("%d",&choix);
 		if (choix == 1){
 		l=300;
@@ -15,13 +16,14 @@ int main(int argc, char * argv[]){
 			l=480;
 			h=270;//Vegeta
 		}
+
 		int taille,taille_lignes;
 		scanf("%d %d",&taille,&taille_lignes);
 	  InitialiserGraphique();
-    CreerFenetre(600,600,700,700);
+    CreerFenetre(600,600,1000,1000);
 		for(i=0;i<h;i+=(h/taille)){
       for(j=0;j<l;j+=(l/taille_lignes)){
-        ChargerImage("Vegeta.jpg",posx,posy,j,i,(l/taille_lignes),(h/taille));
+        ChargerImage(image_selectionnee[choix-1],posx,posy,j,i,(l/taille_lignes),(h/taille));
 				printf("posx=%d , posy=%d , j=%d , i=%d , l=%d , h=%d\n",posx,posy,j,i,(l/taille_lignes),(h/taille));
 				posx+=((l/taille_lignes)+5);
       }
@@ -29,31 +31,6 @@ int main(int argc, char * argv[]){
       j=0;
 			posx=150;
     }
-		/*
-		for(i=0;i<h;i+=(h/taille)){
-      for(j=0;j<l;j+=(l/taille)){
-        ChargerImage("Kangoo.jpg",posx,posy,j,i,(l/taille),(h/taille));
-        printf("posx=%d , posy=%d , j=%d , i=%d\n",posx,posy,j,i);
-        posx+=((l/taille)+5);
-      }
-      posy+=((h/taille)+5);
-      j=0;
-      posx=150;
-    }*/
-		/*
-		ChargerImage("Vegeta.jpg",posx,posy,0,0,160,90);
-		ChargerImage("Vegeta.jpg",posx+=165,posy,160,0,160,90);
-		ChargerImage("Vegeta.jpg",posx+=165,posy,320,0,160,90);
-
-		ChargerImage("Vegeta.jpg",posx=150,posy+=95 ,0,90 ,160,90);
-		ChargerImage("Vegeta.jpg",posx+=165,posy ,160 ,90 ,160,90);
-		ChargerImage("Vegeta.jpg",posx+=165,posy ,320 ,90 ,160,90);
-
-		ChargerImage("Vegeta.jpg",posx=150,posy+=95 ,0,180 ,160,90);
-    ChargerImage("Vegeta.jpg",posx+=165,posy ,160 ,180 ,160,90);
-    ChargerImage("Vegeta.jpg",posx+=165,posy ,320 ,180 ,160,90);
-		*/
-
     Touche();
     FermerGraphique();
 
